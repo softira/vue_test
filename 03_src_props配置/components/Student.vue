3ba0279f06ca8d18febfe669d2ad58f1de8b2580
tanshuai <1160390705@qq.com>
2022-06-14 17:15:49 +0800

    props配置

---
+++ 03_src_props配置/components/Student.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <hr />
+    <h1>name:{{myName}}</h1>
+    <h1>sex:{{mySex}}</h1>
+    <h1>age:{{myAge+1}}</h1>
+    <button @click="myAge++">age++</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'School',
+    data() {
+      return {
+        myName:this.name,
+        mySex:this.sex,
+        myAge:this.age
+      }
+    },
+    // 简单声明接收
+    // props:['name','sex','age'],
+    
+    // 接收的同时对数据进行类型限制
+    // props:{
+    //   name:String,
+    //   sex:String,
+    //   age:Number
+    // }
+    
+    // 接收的同时对数据：进行类型限制+默认值指定+必要性限制
+    props:{
+      name:{
+        type:String,
+        required:true
+      },
+      sex:{
+        type:String,
+        required:true
+      },
+      age:{
+        type:Number,
+        default:22
+      }
+    }
+  }
+</script>
+
+<style>
+  
+</style>
