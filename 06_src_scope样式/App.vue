38a405055d88c4efacbe8f066c8df6f7c749869d
tanshuai <1160390705@qq.com>
2022-06-15 14:42:20 +0800

    scope样式

---
+++ 06_src_scope样式/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="demo">
+    <Student />
+    <School />
+  </div>
+</template>
+
+<script>
+  // 引入School组件
+  import Student from "./components/Student.vue";
+  import School from "./components/School.vue";
+  
+  export default {
+    name: 'App',
+    components:{Student,School},
+  }
+</script>
+
+<style>
+  .demo{
+    margin: 0 auto;
+    width: 500px;
+    background-color: #ccc;
+  }
+</style>
--- README.md
+++ README.md
@@ -94,3 +94,7 @@
     }
   使用插件：
     vue.use()
+
+## scoped样式
+  作用：让样式在局部生效，防止冲突
+  写法：<style scoped>
